14e207daa8a487a94b02911dc7a72289c3baaec7
pauljaemz <pauljames2001@gmail.com>
2025-07-03 20:23:35 +0530

    Brouchure pdf and functions added

--- js/emailjs-init.js
+++ js/emailjs-init.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
         // Send email using EmailJS
         emailjs.send("service_y4wdacc", "template_r9zl5on", {
             to_email: email,
-            brochure_link: window.location.origin + "/brochure.pdf"
         })
         .then(function(response) {
             alert("Brochure sent! Please check your email.");
